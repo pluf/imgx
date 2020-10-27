d46138456414718110c64b3c6ddb8d503522625f
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-10-27 20:24:00 +0330

    Adding test units.

--- tests/ConverterTest.php
+++ tests/ConverterTest.php
@@ -1,15 +1,33 @@
 <?php
 namespace Pluf\Tests;
 
-use Pluf\Http\Request;
+use PHPUnit\Framework\TestCase;
+use Pluf\Http\RequestFactory;
 use Pluf\Imgx\Converter;
 
 class ConverterTest extends TestCase
 {
-    public function resize(){
-        $request = new Request();
+
+    /**
+     *
+     * @test
+     */
+    public function resize()
+    {
+        $requestFactory = new RequestFactory();
+        $request = $requestFactory
+            ->createRequest('GET', '/imgx/api/v2/cms/contents/1/contetn')
+            ->withQueryParams([
+                'w' => 100,
+                'h' => 100
+            ]);
+
+        $target = '/tmp/imgx-test-sample-1-' . rand() . '.jpeg';
+
         $cnv = new Converter();
-        $res = $cnv(__DIR__ . '/assets/sample-1.jpeg', '/tmp/imgx-test-sample-1-' . rand() . '.jpeg');
+        $res = $cnv(__DIR__ . '/assets/sample-1.jpeg', $target, $request);
+
+        $this->assertTrue(is_file($res));
     }
 }
 
