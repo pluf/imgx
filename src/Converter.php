e7032a3e9c92ba25a8bff871f8af5c67f2bdd987
hadi2 <hadi2@linux-y1wf>
2020-10-28 18:13:19 +0330

    intervention/image is added

--- src/Converter.php
+++ src/Converter.php
@@ -3,11 +3,33 @@ namespace Pluf\Imgx;
 
 use Psr\Http\Message\ServerRequestInterface;
 
+// import the Intervention Image Manager Class
+use Intervention\Image\ImageManagerStatic as Image;
+
 class Converter
 {
-    function __invoke(string $origin, string $target, ServerRequestInterface $request){
-        $params = $request->getQueryParams();
-        // TODO: create target base on the given parameters
+    function __invoke(string $origin, string $target, array $params){
+        // open an image file
+        $img = Image::make('public/foo.jpg');
+        
+        switch ($params['f']){
+            case 'cover':
+                
+                break;
+            case 'contain':
+                break;
+            case 'fill':
+            default:
+                break;
+        }
+        // now you are able to resize the instance
+        $img->resize(320, 240);
+        
+        // and insert a watermark for example
+        $img->insert('public/watermark.png');
+        
+        // finally we save the image as a new file
+        $img->save('public/bar.jpg');
         // TODO: write the target into the given target address (if file does not exsited)
         return $target;
     }
